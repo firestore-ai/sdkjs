bb3cfb68533d905bccfd44c8139e94d4b6822803
henrik <houmingyuan@gmail.com>
2024-03-23 14:06:39 +0800

    jsonpath create range

--- word/apiExt.js
+++ word/apiExt.js
@@ -1,24 +1,22 @@
 "use strict";
-(function(window, builder)
-{
-	/**
-	 * Base class
-	 * @global
-	 * @class
-	 * @name Api
-	 */
-	var asc_docs_api = window["Asc"]["asc_docs_api"] || window["Asc"]["spreadsheet_api"];
-	var c_oAscRevisionsChangeType = Asc.c_oAscRevisionsChangeType;
-	var c_oAscSectionBreakType    = Asc.c_oAscSectionBreakType;
-	var c_oAscSdtLockType         = Asc.c_oAscSdtLockType;
-	var c_oAscAlignH         = Asc.c_oAscAlignH;
-	var c_oAscAlignV         = Asc.c_oAscAlignV;
+(function (window, builder) {
+    /**
+     * Base class
+     * @global
+     * @class
+     * @name Api
+     */
+    var asc_docs_api = window["Asc"]["asc_docs_api"] || window["Asc"]["spreadsheet_api"];
+    var c_oAscRevisionsChangeType = Asc.c_oAscRevisionsChangeType;
+    var c_oAscSectionBreakType = Asc.c_oAscSectionBreakType;
+    var c_oAscSdtLockType = Asc.c_oAscSdtLockType;
+    var c_oAscAlignH = Asc.c_oAscAlignH;
+    var c_oAscAlignV = Asc.c_oAscAlignV;
 
     /**
      * Extents the Api class
      */
-    asc_docs_api.prototype.asc_GetContentControlBoundingRectExt = function (id)
-    {
+    asc_docs_api.prototype.asc_GetContentControlBoundingRectExt = function (id) {
         var document = this.private_getDocument();
         var oElement = document.GetContentControlById(id);
 
@@ -29,9 +27,9 @@
                 var PageSection = Page.Sections[SectionIndex];
 
                 for (var ColumnIndex = 0, ColumnsCount = PageSection.Columns.length; ColumnIndex < ColumnsCount; ++ColumnIndex) {
-                    var Column         = PageSection.Columns[ColumnIndex];
+                    var Column = PageSection.Columns[ColumnIndex];
                     var ColumnStartPos = Column.Pos;
-                    var ColumnEndPos   = Column.EndPos;
+                    var ColumnEndPos = Column.EndPos;
 
                     for (var ContentPos = ColumnStartPos; ContentPos <= ColumnEndPos; ++ContentPos) {
                         var oElement = this.Content[ContentPos];
@@ -54,8 +52,8 @@
             }
         }
 
-	    return ccRects;
-	};
+        return ccRects;
+    };
 
     /**
      * update custom xml
@@ -64,7 +62,7 @@
         var customXmls = document.CustomXmls;
         for (var i = 0, n = customXmls.length; i < n; i++) {
             var customXml = customXmls[i];
-            if (customXml.ItemId === id) {                
+            if (customXml.ItemId === id) {
                 var oldContent = customXml.Content;
                 customXml.Content = xml;
                 return oldContent
@@ -87,26 +85,24 @@
     /**
      * Set custom xml     * 
      */
-    asc_docs_api.prototype.asc_SetCustomXmlExt = function(uri, id, xml)
-    {
+    asc_docs_api.prototype.asc_SetCustomXmlExt = function (uri, id, xml) {
         var document = this.private_GetLogicDocument();
         const encoder = new TextEncoder();
         return updateCustomXml(document, uri, id, encoder.encode(xml));
     }
 
-    asc_docs_api.prototype.asc_GetCustomXmlExt = function(id)
-    {
-        var decode = function(customXml) {
-            const decoder = new TextDecoder();    
+    asc_docs_api.prototype.asc_GetCustomXmlExt = function (id) {
+        var decode = function (customXml) {
+            const decoder = new TextDecoder();
             return {
                 Uri: customXml.Uri,
                 ItemId: customXml.ItemId,
-                Content:  String.fromCharCode(...customXml.Content)
+                Content: String.fromCharCode(...customXml.Content)
             }
         }
-        
+
         var document = this.private_GetLogicDocument();
-        if(typeof id === "string") {
+        if (typeof id === "string") {
             var customXmls = document.CustomXmls;
             for (var i = 0, n = customXmls.length; i < n; i++) {
                 var customXml = customXmls[i];
@@ -116,22 +112,22 @@
             }
             return undefined;
         } else if (typeof id === "number") {
-            return decode(document.CustomXmls[id]);            
+            return decode(document.CustomXmls[id]);
         }
 
         return undefined;
     }
-    
+
     // store js object to CDATA in custom xml 
-    var encodeObjToXml = function(obj) { 
-        var xml = "<root><![CDATA[";        
-        xml += JSON.stringify(obj);        
+    var encodeObjToXml = function (obj) {
+        var xml = "<root><![CDATA[";
+        xml += JSON.stringify(obj);
         xml += "]]></root>";
         return xml;
     }
 
     // decode js object from CDATA in custom xml
-    var decodeObjFromXml = function(xml) {
+    var decodeObjFromXml = function (xml) {
         var obj = {};
         var parser = new DOMParser();
         var xmlDoc = parser.parseFromString(xml, "text/xml");
@@ -144,7 +140,7 @@
     // 获取biyue定制数据
     // 如果uuid为空，则返回所有的定制数据
     // 如果uuid不为空，则返回指定的定制数据
-    asc_docs_api.prototype.asc_GetBiyueCustomDataExt = function(uuid)  {
+    asc_docs_api.prototype.asc_GetBiyueCustomDataExt = function (uuid) {
         var uri = "http://nicedoc/schema/question/1.0";
         // return all xmls          
         if (uuid == undefined) {
@@ -154,9 +150,9 @@
             for (var i = 0, n = customXmls.length; i < n; i++) {
                 var customXml = this.asc_GetCustomXmlExt(i);
                 // if uri in uri list                
-                if (customXml.Uri.includes(uri)) {                                                            
-                    result.push({"ItemId": customXml.ItemId, "Content": decodeObjFromXml(customXml.Content)});
-                }                
+                if (customXml.Uri.includes(uri)) {
+                    result.push({ "ItemId": customXml.ItemId, "Content": decodeObjFromXml(customXml.Content) });
+                }
             }
             return result;
         } else {
@@ -173,7 +169,7 @@
     // 如果uuid为空，则创建新的定制数据
     // 如果uuid不为空，则更新指定的定制数据
     // 返回uuid
-    asc_docs_api.prototype.asc_SetBiyueCustomDataExt = function(uuid, data)  {
+    asc_docs_api.prototype.asc_SetBiyueCustomDataExt = function (uuid, data) {
         if (uuid == undefined) {
             uuid = window.AscCommon.CreateGUID();
         }
@@ -182,12 +178,125 @@
         this.asc_SetCustomXmlExt(uris, uuid, xml);
         return uuid;
     }
-    
+
+    function ProcessNode(element, handler) {
+        if (element === undefined || element === null) {
+            return;
+        }
+
+        const cname = element.GetClassType()
+        //const id = element.Id;
+
+        let getText = function (element) {
+            if (element.GetClassType() === "run") {
+                return element.GetText();
+            }
+            return undefined;
+        }
+
+        handler.onStartElement(cname, getText(element));
+        if (element.GetElementsCount != undefined) {
+            for (var i = 0, n = element.GetElementsCount(); i < n; i++) {
+                ProcessNode(element.GetElement(i), handler);
+            }
+        }
+        handler.onEndElement(cname);
+    }
+
+    // 遍历文档dom结构
+    asc_docs_api.prototype.asc_StartSaxParse = function (element, handler) {
+        ProcessNode(element, handler);
+    }
+
+    asc_docs_api.prototype.asc_RunSaxTest = function () {
+        var handler = {
+            onStartElement: function (cname, text) {
+                console.log("onStartElement: " + cname + " " + (text || ""));
+            },
+            onEndElement: function (cname) {
+                console.log("onEndElement: " + cname + " ");
+            }
+        }
+
+        this.asc_StartSaxParse(this.GetDocument(), handler);
+    }
+
+    asc_docs_api.prototype.asc_MakeRangeByPath = function (beg, end) {
+        if (typeof beg === 'number' && typeof end === 'number')
+            return Api.GetDocument().GetRange().GetRange(beg, end);
+        // make range by path
+
+        function toClass(node) {
+            if (node.GetClassType() === "run") {
+                return node.Run;
+            } else if (node.GetClassType() === "paragraph") {
+                return node.Paragraph;
+            } else if (node.GetClassType() === "table") {
+                return node.Table;
+            } else if (node.GetClassType() === "cell") {
+                return node.Cell;
+            } else if (node.GetClassType() === "row") {
+                return node.Row;
+            } else if (node.GetClassType() === "document") {
+                return node.Document;
+            }
+            return undefined;
+        }
+
+        var begIdxs = beg.match(/([-]?\d+)/g).map(e => parseInt(e));
+        var endIdxs = end.match(/([-]?\d+)/g).map(e => parseInt(e));
+        var startPos = []
+        var endPos = []
+
+        var currNode = this.private_GetLogicDocument();
+        startPos = begIdxs.map(e => {
+            // is content array
+            if (!currNode.Content.length) {
+                currNode = currNode.Content;                
+            }
+
+            if (e < 0) {
+                e = currNode.Content.length  + e;
+            }            
+
+            var r  = {
+                Class: currNode,
+                Position: e
+            };            
+            currNode = currNode.Content[e];
+            return r;
+        });
+
+        var currNode = this.private_GetLogicDocument();
+        endPos = endIdxs.map(e => {
+            if (!currNode.Content.length) {
+                currNode = currNode.Content;                
+            }
+
+            if (e < 0) {
+                e = currNode.Content.length  + e;
+            }            
+
+            var r =   {
+                Class: currNode,
+                Position: e
+            };   
+            currNode = currNode.Content[e];
+            return r;            
+        });
+
+        return  this.GetDocument().GetRange(startPos, endPos);
+    }
+
+
     asc_docs_api.prototype["asc_GetContentControlBoundingRectExt"] = asc_docs_api.prototype.asc_GetContentControlBoundingRectExt;
     asc_docs_api.prototype["asc_SetCustomXmlExt"] = asc_docs_api.prototype.asc_SetCustomXmlExt;
     asc_docs_api.prototype["asc_GetCustomXmlExt"] = asc_docs_api.prototype.asc_GetCustomXmlExt;
     asc_docs_api.prototype["asc_SetBiyueCustomDataExt"] = asc_docs_api.prototype.asc_SetBiyueCustomDataExt;
     asc_docs_api.prototype["asc_GetBiyueCustomDataExt"] = asc_docs_api.prototype.asc_GetBiyueCustomDataExt;
+    asc_docs_api.prototype["asc_StartSaxParse"] = asc_docs_api.prototype.asc_StartSaxParse;
+
+    asc_docs_api.prototype["asc_MakeRangeByPath"] = asc_docs_api.prototype.asc_MakeRangeByPath;
 
 
 }(window, null));
